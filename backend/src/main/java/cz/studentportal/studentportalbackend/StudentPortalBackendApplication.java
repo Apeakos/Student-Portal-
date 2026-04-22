2db6b8115b45cc3a586e5a038487c1642a6cdedd
Apeakos <apik2905@seznam.cz>
2026-04-22 20:46:06 +0200

    Funkční SpringBoot+StudentController

--- backend/src/main/java/cz/studentportal/studentportalbackend/StudentPortalBackendApplication.java
+++ backend/src/main/java/cz/studentportal/studentportalbackend/StudentPortalBackendApplication.java
@@ -2,12 +2,17 @@ package cz.studentportal.studentportalbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StudentPortalBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentPortalBackendApplication.class, args);
-    }
+        ConfigurableApplicationContext ctx = SpringApplication.run(StudentPortalBackendApplication.class, args);
 
+        System.out.println("\n========= DEBUG ENV =========");
+        System.out.println("SPRING_DATASOURCE_URL (ENV): " + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("spring.datasource.url (Property): " + ctx.getEnvironment().getProperty("spring.datasource.url"));
+        System.out.println("=============================\n");
+    }
 }
