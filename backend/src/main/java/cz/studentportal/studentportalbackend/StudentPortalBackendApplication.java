e8543bab080e2cce8ea5e3bb68394470cbe1ca7b
Apeakos <apik2905@seznam.cz>
2026-04-22 19:38:47 +0200

    Přidání souborů do adresářů

---
+++ backend/src/main/java/cz/studentportal/studentportalbackend/StudentPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.studentportal.studentportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPortalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPortalBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cz/studentportal/studentportalbackend/StudentPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.studentportal.studentportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
