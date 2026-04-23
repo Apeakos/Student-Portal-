05ff6732413fdb8198cd0b27f1b8ed84993bf702
Apeakos <apik2905@seznam.cz>
2026-04-23 13:38:52 +0200

    Přidání init.sql pro test

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS student (
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255)
+    );
+
+INSERT INTO student (first_name, last_name, email) VALUES
+ ('Jan', 'Novák', 'jan@example.com'),
+ ('Petr', 'Svoboda', 'petr@example.com');
