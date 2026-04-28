e7b9d0570e027924568a2e2727f7ee4d7210fe89
R24131 <pezdja41@osu.cz>
2026-04-28 15:28:47 +0200

    Login page + uprava App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './Login';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <Login onLogin={(userData) => setUser(userData)} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ padding: '30px', fontFamily: 'sans-serif' }}>
+        <h1>Vítej v portálu, {user.firstName} {user.lastName}!</h1>
+        <p>Tvůj email: {user.email}</p>
+
+        <hr style={{ margin: '20px 0' }} />
+
+        <h3>Tvoje předměty:</h3>
+        <p><i>(Tady v dalším kroku přidáme tabulku se známkami z databáze)</i></p>
+
+        <button
+            onClick={() => setUser(null)}
+            style={{ marginTop: '20px', padding: '5px 15px', color: 'red' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Odhlásit se
+        </button>
+      </div>
   );
 }
 
